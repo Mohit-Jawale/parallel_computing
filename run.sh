86b3bb4cf690d91b61339703297765416bba7bc0
Mohit Jawale <jawale.mohit123@gmail.com>
2022-12-06 13:32:09 -0800

    update sh

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+module load CMake
+
 module switch GCCcore GCC
 
 module load OpenMPI
@@ -10,30 +12,23 @@ make dijkstra
 
 
 #
-#SBATCH --job-name=matmul
+#SBATCH --job-name=djiktras
 #SBATCH --partition=cpu
 #SBATCH --cpus-per-task=8
 #SBATCH --mem-per-cpu=8G
 #SBATCH --nodes=1
-#SBATCH --output=ass_3-%j.out
+#SBATCH --output=ass_3_20000-%j.out
 #SBATCH --time=1000:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=mjawale@scu.edu
 #
-export OMP_NUM_THREADS=1
+export OMP_NUM_THREADS=8
 export OMP_PLACES=cores
 export OMP_PROC_BIND=spread
 
 
 
-srun ./matmult_omp 1000 1000 1000 20
-
-
-
-
-
-
-
+srun ./dijkstra /WAVE/projects/COEN-319-Fa22/data/pr3/20000.graph 20000 output_20000
 
 
 
