887d7758f11914a7a5d0d6fd6530be485cdc81ca
Mohit Jawale <jawale.mohit123@gmail.com>
2022-12-06 13:40:57 -0800

    update sh

---
+++ algo_run.sh
@@ -0,0 +1,17 @@
+#
+#SBATCH --job-name=djiktras
+#SBATCH --partition=cpu
+#SBATCH --cpus-per-task=8
+#SBATCH --mem-per-cpu=8G
+#SBATCH --nodes=1
+#SBATCH --output=ass_3_20000-%j.out
+#SBATCH --time=1000:00
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=mjawale@scu.edu
+#
+export OMP_NUM_THREADS=8
+export OMP_PLACES=cores
+export OMP_PROC_BIND=spread
+
+
+srun ./dijkstra /WAVE/projects/COEN-319-Fa22/data/pr3/20000.graph 20000 output_20000
--- run.sh
+++ run.sh
@@ -10,30 +10,12 @@ git pull https://github.com/Mohit-Jawale/parallel_computing.git
 
 make dijkstra
 
-
-#
-#SBATCH --job-name=djiktras
-#SBATCH --partition=cpu
-#SBATCH --cpus-per-task=8
-#SBATCH --mem-per-cpu=8G
-#SBATCH --nodes=1
-#SBATCH --output=ass_3_20000-%j.out
-#SBATCH --time=1000:00
-#SBATCH --mail-type=ALL
-#SBATCH --mail-user=mjawale@scu.edu
-#
-export OMP_NUM_THREADS=8
-export OMP_PLACES=cores
-export OMP_PROC_BIND=spread
-
-
-
-srun ./dijkstra /WAVE/projects/COEN-319-Fa22/data/pr3/20000.graph 20000 output_20000
-
-
-
-
 make clean
+
+
+
+
+
 
 
 
